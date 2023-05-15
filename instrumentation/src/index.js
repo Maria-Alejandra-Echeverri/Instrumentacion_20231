62ae4735906aa7881d05b318a26acdb16779f17d
Alejandra_Echeverri <alejandraecheverri810@gmail.com>
2023-05-15 15:22:43 -0500

    Base de datos

--- instrumentation/src/index.js
+++ instrumentation/src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const { send } = require('express/lib/response');
 const app = express(); //server
+require('dotenv').config();
 
 //Asignacion de puerto
 const port = process.env.PORT || 3000;
+
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+    next();
+})
+
+
 //Llamar la app
 app.use((req, res) =>{
-    res.send('Hi!')
+    res.send('Hi, Im Aleja!')
 })
 //Configuracion
 app.set('port', port)
